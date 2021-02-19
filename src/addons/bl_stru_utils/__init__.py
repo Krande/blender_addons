4ddc4c7cc8c9ff55730f9062b09507b6975faa80
ofskrand <kristoffer_andersen@outlook.com>
2021-02-19 15:10:22 +0100

    minor edit [skip ci]

--- src/addons/bl_stru_utils/__init__.py
+++ src/addons/bl_stru_utils/__init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Structural Utils",
     "description": "Utilities that I needed and that others might find useful",
     "author": "Kristoffer H. Andersen",
-    "blender": (2, 90, 1),
+    "blender": (2, 91, 2),
     "version": (0, 0, 99),
     "location": "View3D",
     "category": "Development",
